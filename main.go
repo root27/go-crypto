d5e703f561e9e45ff0f7acef686352e92c5cc63d
root27 <oguzhaneee@gmail.com>
2023-12-13 15:56:33 +0300

    remove bash script

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/rodaine/table"
@@ -22,9 +23,51 @@ const (
 	ColorReset        = "\u001b[0m"
 )
 
+var script = `
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+
+# Simulate a loading effect
+chars="
+
+ ################## Welcome to Coin App ##################
+ 
+
+    Usage with arguments:
+    "
+
+greenChars=" 
+        --help, -help: show this help message and exit
+        --coin, -coin [ARG]: show coin info
+        
+            Exampe: -coin Bitcoin
+
+        --all, -all: show first 500 coins
+        --number, -number [ARG]: Number of coins to display. Use with -all flag 
+        
+            Example: -number 50 -all or -all -number 50"
+
+for (( j=0; j<${#chars}; j++ )); do
+    printf "${chars:$j:1}"
+    sleep 0.005
+done
+
+for (( j=0; j<${#greenChars}; j++ )); do
+    printf "${GREEN}${greenChars:$j:1}${ENDCOLOR}"
+    sleep 0.005
+done
+
+printf "\n"
+
+`
+
 func WelcomeMessage() {
 
-	cmd := exec.Command("bash", "-c", "./welcome.sh")
+	cmd := exec.Command("bash")
+
+	cmd.Stdin = strings.NewReader(script)
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
